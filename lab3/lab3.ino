912d3475a48e73a9c918e7ded742e8643850b08a
Sean Herman <seanherman@gmail.com>
2015-10-16 11:27:49 -0400

    scaffolding

---
+++ lab3/lab3.ino
@@ -0,0 +1,15 @@
+typedef struct Moves {
+  int angleStart;
+  int angleEnd;
+  int angleStep;
+} Move;
+
+void setup() {
+
+}
+
+void loop() {
+  while (Serial.available() > 0) {
+    // TODO
+  }
+}
